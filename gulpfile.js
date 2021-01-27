4035a734cf6d9513d63a3ffc3be5d4387dd08498
TheKid <thekid@iMac-de-TheKid.local>
2021-01-27 12:22:30 -0300

    init ficha falabella

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// npm init to create package.json file
+// don't forget to install gulp as a dev dependency for this project, and also globally
+
+var   gulp = require('gulp'),
+      concat = require('gulp-concat'),
+      del = require('del'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create();
+
+gulp.task('compileSass', function() {
+  return gulp.src("scss/minimal.scss")
+      .pipe(sass())
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task("concatIndex", function() {
+  gulp.src(['partials/header.html', 'partials/index.html', 'partials/footer.html'])
+  .pipe(concat("index.html"))
+  .pipe(gulp.dest("public/"))
+  .pipe(browserSync.stream());
+});
+
+
+gulp.task("watch", function() {
+  browserSync.init({
+      server: {
+          baseDir: "./public"
+      }
+  });
+
+  return gulp.watch(['partials/*.html', 'scss/*', 'img/*', 'js/*'], ['build'])
+
+})
+
+gulp.task('clean', function() {
+  del(['public/static/RDF/site/content/landing/samsung-2018'])
+});
+
+gulp.task("build", ['compileSass', 'concatIndex'], function() {
+  return gulp.src(["css/*","fonts/*", "img/*", "js/*"], { base: './'})
+  .pipe(gulp.dest('public/static/RDF/site/content/landing/samsung-2018'))
+  .pipe(browserSync.stream());
+});
+
+
+
+
+gulp.task("default", ["watch"]);
+
+// /public/static/RDF/site/content/landing/samsung-2018
